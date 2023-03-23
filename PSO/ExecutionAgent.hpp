b95e53fb95906300383e565ec07b05ca75a4e834
“gaoyuqi” <gaoyuqi@toki.waseda.jp>
2023-03-24 03:17:09 +0900

    添加get函数辅助构造Assist类

--- PSO/ExecutionAgent.hpp
+++ PSO/ExecutionAgent.hpp
@@ -16,11 +16,23 @@
 using namespace std;
 class ExecutionAgent{
 public:
+    ExecutionAgent(); //读文件，对jobs，machines进行初始化
+    vector<Job> get_jobs() const{
+        return jobs;
+    }
+    vector<Machine> get_machines() const{
+        return machines;
+    }
+    //辅助构造Assit类
+    int get_operationsNumber() const{
+        return operations_n;
+    }
+private:
     vector<Job> jobs;
     vector<Machine> machines;
     int jobs_n;
     int machines_n;
-    ExecutionAgent(); //读文件，对jobs，machines进行初始化
+    int operations_n;
     void splitStr(vector<int> &arr, string &s);//分割字符串，将每个数字存入vector arr
 };
 #endif /* ExecutionAgent_hpp */
