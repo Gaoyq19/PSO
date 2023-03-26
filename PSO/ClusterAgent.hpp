38df1a3976d99b9fa54c8e1670ef5e438ae4a094
“gaoyuqi” <gaoyuqi@toki.waseda.jp>
2023-03-27 04:42:37 +0900

    CA类

--- PSO/ClusterAgent.hpp
+++ PSO/ClusterAgent.hpp
@@ -11,10 +11,30 @@
 
 #include <stdio.h>
 #include "Particle.hpp"
+using namespace std;
 class ClusterAgent{
 private:
-    vector<Particle> particles;
-    Particle localBest;
+    vector<shared_ptr<Particle>> particles;
+    shared_ptr<Particle> particle_LB;
+    int makespan_LB;
 public:
+    void add_particle(shared_ptr<Particle> p){
+        particles.push_back(p);
+        int makespan = p->get_makespan();
+        if (makespan_LB > makespan) {
+            particle_LB = p;
+            makespan_LB = makespan;
+        }
+    }
+    void calculate(Assist &assist){
+        for (auto i : particles) {
+            i->calculate(assist);
+        }
+    }
+    void update(){
+        for (auto i : particles) {
+            i->update();
+        }
+    }
 };
 #endif /* ClusterAgent_hpp */
